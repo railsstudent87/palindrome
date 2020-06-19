d0465a98b5a33a414472043581947f7e7c84fcf2
Rails Student <railsstudent@pm.me>
2020-06-19 18:20:50 +0000

    Initialize repo

---
+++ index.js
@@ -0,0 +1,49 @@
+// Reverses a string.
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Adds `blank` to all strings.
+String.prototype.blank = function() {
+  return !!this.match(/\s+/gm);
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+  
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+  
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+  
+  // Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+  
+  this.louder = function louder() {
+    return this.content.toUpperCase()
+  }
+}
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+  
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
